b54ff098baf4f7d0a23c51a080b8aa3a2e1ca9db
Divyanshu Tyagi <divyanshutyagi@Divyanshus-MacBook-Pro.local>
2024-03-14 11:25:19 +0530

    String Started

---
+++ APNA COLLEGE/Strings/String1.java
@@ -0,0 +1,8 @@
+
+class Strings {
+    public static void main(String[] args) {
+        // ! Creating String:
+        String str = "Divyanshu";
+        String str2 = new String("Navjot");
+    }
+}
